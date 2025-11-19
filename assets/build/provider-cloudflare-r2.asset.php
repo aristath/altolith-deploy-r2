0b8e2402a21c5bbf4babaf5fd13cd16a13697d6c
Ari Stathopoulos <aristath@gmail.com>
2025-11-19 12:46:18 +0200

    Rebuild assets after provider refactoring
    - Rebuild Cloudflare R2 provider assets
    - Include modal hooks in build

--- assets/build/provider-cloudflare-r2.asset.php
+++ assets/build/provider-cloudflare-r2.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => 'e5389cf0f085561ccd44');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => 'eade71a79d21a5b406dd');
